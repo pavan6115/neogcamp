e530db167795b5ae17150b9a4cef6ac2917589bb
kpavann <kulkarnipavan6115@gmail.com>
2021-05-18 11:41:21 +0530

    updated for event listeners

---
+++ banana-speaks/script.js
@@ -0,0 +1,7 @@
+let btnTranslate = document.querySelector('#btnTranslate')
+let txt = document.querySelector('#txt')
+
+btnTranslate.addEventListener('click', () => {
+    console.log('hey new click')
+    console.log(txt.value)
+})
