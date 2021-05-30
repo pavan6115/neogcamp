1d9e42acb14264c728caff144b30a8530f29936b
kpavann <kulkarnipavan6115@gmail.com>
2021-05-30 12:08:25 +0530

    added translation functionality

--- banana-speaks/script.js
+++ banana-speaks/script.js
@@ -1,9 +1,30 @@
 let btnTranslate = document.querySelector('#btnTranslate')
 let txtarea = document.querySelector('#txtarea')
 let outputDiv = document.querySelector('.output')
+let serverURL = 'https://api.funtranslations.com/translate/ferb-latin.json'
+
+// let translation = text => serverURL + "?" + "text=" + txtarea
+// let errorhandling = err => alert('Something wrong with the server, check back later 🥺')
+ 
+function translation(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorhandling(err) {
+    console.log('Error occured - ', err)
+    alert('Something wrong with the server, check back later 🥺')
+}
+
 
 btnTranslate.addEventListener('click', () => {
-    outputDiv.innerText = 'gibberish lang ' + txtarea.value
+    let inputText = txtarea.value //taking input from textarea
+    
+    // calling server for processing
+    fetch(translation(inputText))
+        .then(response => response.json())
+        .then(json => {
+            let translatedtext = json.contents.translated;
+            outputDiv.innerText = translatedtext //output
+         })
+        .catch(errorhandling)
 })
-
-outputDiv.innerText = 'Yoo whatisup whatisup bantai!'
