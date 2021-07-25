2b521406e9d4fa45c1cf85492cec7665bc6e2d3a
kpavann <kulkarnipavan6115@gmail.com>
2021-07-25 17:05:28 +0530

    practice file

--- markOne/index.js
+++ markOne/index.js
@@ -1,129 +1,129 @@
 const readlineSync = require('readline-sync')
 
-// const userName = readlineSync.question('What is your name? ')
-// console.log('Welcome ' + userName + '!')
+const userName = readlineSync.question('What is your name? ')
+console.log('Welcome ' + userName + '!')
 
 
-// console.log("Let's see if you are older than 25 yrs")
-// const inpAge = readlineSync.question('Enter your age - ')
-// console.log('You have entered ' + inpAge + 'yrs')
-// const answer = (inpAge >= 25) ? "Yes, you're right" : "No, you're wrong"
-// console.log(answer)
+console.log("Let's see if you are older than 25 yrs")
+const inpAge = readlineSync.question('Enter your age - ')
+console.log('You have entered ' + inpAge + 'yrs')
+const answer = (inpAge >= 25) ? "Yes, you're right" : "No, you're wrong"
+console.log(answer)
 
 
-// let score = 0
-// const ansOne = 'Panvel'
-// const queOne = readlineSync.question('Enter your city name - ')
-// if (queOne === ansOne) {
-//     console.log('We are from same city!!')
-//     score += 1
-//     console.log('Score is: ', score)
-// }
-// else {
-//     console.log('We are from different city')
-//     score -= 1
-//     console.log('Score is: ', score)
-// }
+let score = 0
+const ansOne = 'Panvel'
+const queOne = readlineSync.question('Enter your city name - ')
+if (queOne === ansOne) {
+    console.log('We are from same city!!')
+    score += 1
+    console.log('Score is: ', score)
+}
+else {
+    console.log('We are from different city')
+    score -= 1
+    console.log('Score is: ', score)
+}
 
-// const ansTwo = 'Iron Man'
-// const queTwo = readlineSync.question('Enter your favorite Marvel character : ')
-// if (queTwo === ansTwo) {
-//     console.log('Yea, I like Iron Man too')
-//     score += 1
-//     console.log('Score is: ', score)
-// }
-// else {
-//     console.log('No, I like Iron Man more')
-//     score -= 1
-//     console.log('Score is: ', score)
-// }
-
-
-// function add(num1, num2){
-//     console.log('The sum of', num1, 'and', num2, 'is', num1+num2)
-// }
-// add(4,7)
-
-// function prod(num1, num2){
-//     console.log('The product of', num1, 'and', num2, 'is', num1*num2)
-// }
-// prod(4,8)
+const ansTwo = 'Iron Man'
+const queTwo = readlineSync.question('Enter your favorite Marvel character : ')
+if (queTwo === ansTwo) {
+    console.log('Yea, I like Iron Man too')
+    score += 1
+    console.log('Score is: ', score)
+}
+else {
+    console.log('No, I like Iron Man more')
+    score -= 1
+    console.log('Score is: ', score)
+}
 
 
-// let playScore = 0
-// function play (playQue, playAns) {
-//     const userAns = readlineSync.question(playQue)
-//     if (userAns === playAns) {
-//         console.log('You are right!')
-//         playScore += 1
-//         console.log('Score is : ',playScore)
-//     }
-//     else {
-//         console.log('You are wrong!')
-//         playScore -= 1
-//         console.log('Score is : ',playScore)
-//     }
-// }
-// play("My favorite Marvel character? ", "Iron Man")
-// play("Which is my favorite Car? ", "Honda City")
+function add(num1, num2){
+    console.log('The sum of', num1, 'and', num2, 'is', num1+num2)
+}
+add(4,7)
+
+function prod(num1, num2){
+    console.log('The product of', num1, 'and', num2, 'is', num1*num2)
+}
+prod(4,8)
+
+
+let playScore = 0
+function play (playQue, playAns) {
+    const userAns = readlineSync.question(playQue)
+    if (userAns === playAns) {
+        console.log('You are right!')
+        playScore += 1
+        console.log('Score is : ',playScore)
+    }
+    else {
+        console.log('You are wrong!')
+        playScore -= 1
+        console.log('Score is : ',playScore)
+    }
+}
+play("My favorite Marvel character? ", "Iron Man")
+play("Which is my favorite Car? ", "Honda City")
 
 
 // For Loops
-// for(let i=0; i<5; i++) {
-//     console.log(i, 'Pavan Kulkarni')
-// }
+for(let i=0; i<5; i++) {
+    console.log(i, 'Pavan Kulkarni')
+}
 
-// for (let j=0; j<11; j++){
-//     let a = 22
-//     console.log(a + j)
-// }
+for (let j=0; j<11; j++){
+    let a = 22
+    console.log(a + j)
+}
 
-// const groceryList = ["milk", "panner", "vadapav", "carrots", "flowers"]
-// console.log(groceryList[0])
-// console.log(groceryList[2])
-// console.log(groceryList[4])
-// console.log('The length of array is - ', groceryList.length)
+const groceryList = ["milk", "panner", "vadapav", "carrots", "flowers"]
+console.log(groceryList[0])
+console.log(groceryList[2])
+console.log(groceryList[4])
+console.log('The length of array is - ', groceryList.length)
 
-// console.log('Items in grocery list are - ')
-// for (let item in groceryList) {
-//     console.log(groceryList[item])
-// }
+console.log('Items in grocery list are - ')
+for (let item in groceryList) {
+    console.log(groceryList[item])
+}
 
 
 // Objects
-// const ironMan = {
-//     name : 'Iron Man',
-//     power : 10000000,
-//     suitColor : 'Red',
-// }
-// console.log(ironMan.power)
+const ironMan = {
+    name : 'Iron Man',
+    power : 10000000,
+    suitColor : 'Red',
+}
+console.log(ironMan.power)
 
-// const superHeros = [
-//     {
-//         name : 'Iron Man',
-//         power : 10000000,
-//         suitColor : 'red',
-//         partofMCU : 'Yes'
-//     },
-//     {
-//         name : 'Captain America',
-//         power : 2000,
-//         suitColor : 'blue',
-//         partofMCU : 'Yes'
-//     },
-//     {
-//         name : 'Flash',
-//         power : 40000,
-//         suitColor : 'maroon',
-//         partofMCU : 'No'
-//     }
-// ]
+const superHeros = [
+    {
+        name : 'Iron Man',
+        power : 10000000,
+        suitColor : 'red',
+        partofMCU : 'Yes'
+    },
+    {
+        name : 'Captain America',
+        power : 2000,
+        suitColor : 'blue',
+        partofMCU : 'Yes'
+    },
+    {
+        name : 'Flash',
+        power : 40000,
+        suitColor : 'maroon',
+        partofMCU : 'No'
+    }
+]
 
-// for (let superHero in superHeros) {
-//     console.log('Name of Super Hero -', superHeros[superHero].name)
-//     console.log('Suit Color of Super Hero -', superHeros[superHero].suitColor)
-//     console.log('-----------------------------------')
-// }
+for (let superHero in superHeros) {
+    console.log('Name of Super Hero -', superHeros[superHero].name)
+    console.log('Suit Color of Super Hero -', superHeros[superHero].suitColor)
+    console.log('-----------------------------------')
+}
 
 // Making a fun quiz game - 
 let quizScore = 0
