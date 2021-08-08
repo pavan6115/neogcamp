d3d40e8ccec2864d3551d388c2d4c8f5564a26bc
kpavann <kulkarnipavan6115@gmail.com>
2021-08-08 11:50:50 +0530

    style:added new color

--- markTwo/assignmentOne.js
+++ markTwo/assignmentOne.js
@@ -7,47 +7,47 @@ const playerName = readlineSync.question('Enter your name - ')
 console.log('Welcome, ' + playerName + '!')
 
 function quiz(question, answer) {
-    const playerAns = readlineSync.question(question)
-    if (playerAns.toLowerCase() === answer.toLowerCase()) {
-        quizScore += 2
-        console.log(chalk.green('Right Answer!'))
-    }
-    else {
-        quizScore -= 1
-        console.log(chalk.red('Wrong Answer!'))
-    }
-    console.log(chalk.blueBright('Current Score : ' + quizScore))
+  const playerAns = readlineSync.question(question)
+  if (playerAns.toLowerCase() === answer.toLowerCase()) {
+    quizScore += 2
+    console.log(chalk.green('Right Answer!'))
+  } else {
+    quizScore -= 1
+    console.log(chalk.red('Wrong Answer!'))
+  }
+  console.log(chalk.blueBright('Current Score : ' + quizScore))
 }
 
 const questions = [
-    {
-        question : 'Who is the film based on? ',
-        answer : 'Alan Turing'
-    }, 
-    {
-        question : 'What was the name of the machine that was designed by Alan Turing? ',
-        answer : 'Christoper'
-    },
-    {
-        question : 'Where did Alan Turing work during his service? ',
-        answer : 'Bletchley Park'
-    }, 
-    {
-        question : 'What were the common words in every message? ',
-        answer : 'Heil Hitler'
-    },
-    {
-        question : "At what time did the German's send the weather report? ",
-        answer : '6am'
-    },
-    {
-        question : 'Who played the role of Alan Turing? ',
-        answer : 'Benedict Cumberbatch'
-    }
+  {
+    question: 'Who is the film based on? ',
+    answer: 'Alan Turing',
+  },
+  {
+    question:
+      'What was the name of the machine that was designed by Alan Turing? ',
+    answer: 'Christoper',
+  },
+  {
+    question: 'Where did Alan Turing work during his service? ',
+    answer: 'Bletchley Park',
+  },
+  {
+    question: 'What were the common words in every message? ',
+    answer: 'Heil Hitler',
+  },
+  {
+    question: "At what time did the German's send the weather report? ",
+    answer: '6am',
+  },
+  {
+    question: 'Who played the role of Alan Turing? ',
+    answer: 'Benedict Cumberbatch',
+  },
 ]
 
 for (let ques in questions) {
-    quiz(questions[ques].question, questions[ques].answer)
+  quiz(questions[ques].question, questions[ques].answer)
 }
 
 console.log(chalk.yellow(playerName + ' your final Score is : ', quizScore))
