9de5e0461d172e45e76c484cb4bd8d7f5c45d298
kpavann <kulkarnipavan6115@gmail.com>
2021-08-23 14:01:38 +0530

    done

---
+++ interviewPrep/question12/script.js
@@ -0,0 +1,27 @@
+console.log('connected!');
+
+const passwordInp = document.querySelector('#password')
+const checkBtn = document.querySelector('#check-btn')
+const outputMsg = document.querySelector('.outputMsg')
+
+
+function checkPassword(){
+    let pass = passwordInp.value;
+    for (let p = 0; p < pass.length; p++){
+        if (pass.length < 10){
+            checkBtn.disabled = true
+            passwordInp.style.borderColor = "red"
+            outputMsg.style.color = "red"
+            outputMsg.innerText = "Password is less than 10 characters."
+        }
+        else {
+            checkBtn.disabled = false
+            passwordInp.style.borderColor = "green"
+            outputMsg.style.color = "green"
+            outputMsg.innerText = "yayaya!!"
+        }
+    }
+}
+
+
+passwordInp.addEventListener('change', checkPassword)
